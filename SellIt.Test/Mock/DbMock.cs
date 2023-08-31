499fffc56d9ad4d2f1300d3dd7fcb8703389edcb
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2023-08-31 18:56:42 +0300

    .

---
+++ SellIt.Test/Mock/DbMock.cs
@@ -0,0 +1,20 @@
+﻿namespace SellIt.Test.Mock
+{
+    using Microsoft.EntityFrameworkCore;
+    using SellIt.Infrastructure.Data;
+
+    public class DbMock
+    {
+        public static ApplicationDbContext Instance 
+        {
+            get
+            {
+                var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+
+                return new ApplicationDbContext(dbContext);
+            }
+        }
+    }
+}
---
+++ SellIt.Test/Mock/UserServiceMock.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SellIt.Test.Mock
+{
+    using Moq;
+    using SellIt.Core.Contracts.User;
+
+    public class UserServiceMock
+    {
+        public static IUserService Instance
+        {
+            get
+            {
+                var userServiceMock = new Mock<IUserService>();
+
+                userServiceMock
+                    .Setup(u => u.CurrentUserAccessor())
+                    .Returns("UserId1");
+
+                userServiceMock
+                    .Setup(u => u.CurrentUserName())
+                    .Returns("User1");
+
+                return userServiceMock.Object;
+            }
+        }
+    }
+}
